64080f4b6c99590a317c7c827e79bcf432032e9c
Alexandra Ginju <alexandra.ginju@fortech.ro>
2022-06-02 16:12:10 +0300

    Remove setUp(), make sound val

--- app/src/test/java/com/example/beatboxapp/SoundTest.kt
+++ app/src/test/java/com/example/beatboxapp/SoundTest.kt
@@ -1,26 +1,19 @@
 package com.example.beatboxapp
 
-import org.junit.Assert.assertFalse
-import org.junit.Before
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class SoundTest {
 
-    @Mock
-    private lateinit var sound: Sound
-
-    @Before
-    fun setUp() {
-    }
+    private val sound: Sound = Sound("assetPath.wav")
 
     @Test
     fun verifySoundName() {
-        sound = Sound("assetPath.wav")
-        assertFalse(sound.name.contains(".wav"))
+        assertThat(sound.name, CoreMatchers.`is`("assetPath"))
     }
 }
 
